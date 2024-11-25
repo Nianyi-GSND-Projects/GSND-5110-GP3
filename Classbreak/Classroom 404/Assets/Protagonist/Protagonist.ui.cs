0cc8465dc272c50e0b5279538775ed044b1fe1e7
Nianyi Wang <wangnianyi2001@outlook.com>
2024-11-25 15:42:55 -0500

    Implement the starting eyelid animations

---
+++ Classbreak/Classroom 404/Assets/Protagonist/Protagonist.ui.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace Game
+{
+	public partial class Protagonist
+	{
+		[Header("UI")]
+		[SerializeField] private RawImage eyelids;
+		[SerializeField][Min(0)] private float eyelidSpeed = 1.0f;
+
+		public float EyelidOpenness {
+			get {
+				return eyelids.material.GetFloat("_Openness");
+			}
+			set
+			{
+				value = Mathf.Clamp01(value);
+				eyelids.material.SetFloat("_Openness", value);
+				eyelids.enabled = value != 1.0f;
+			}
+		}
+
+		public void SetEyelidOpenness(float targetOpenness) {
+			if(setEyelidOpennessCoroutine != null)
+			{
+				StopCoroutine(setEyelidOpennessCoroutine);
+				setEyelidOpennessCoroutine = null;
+			}
+			StartCoroutine(SetEyelidOpennessCoroutine(targetOpenness));
+		}
+
+		private Coroutine setEyelidOpennessCoroutine;
+		private IEnumerator SetEyelidOpennessCoroutine(float targetOpenness)
+		{
+			float startOpenness = EyelidOpenness;
+			float totalTime = Mathf.Abs((startOpenness - targetOpenness) / eyelidSpeed);
+			for(float startTime = Time.time, elapsed; (elapsed = Time.time - startTime) < totalTime;)
+			{
+				EyelidOpenness = Mathf.Lerp(startOpenness, targetOpenness, elapsed / totalTime);
+				yield return new WaitForEndOfFrame();
+			}
+			EyelidOpenness = targetOpenness;
+			setEyelidOpennessCoroutine = null;
+		}
+	}
+}
