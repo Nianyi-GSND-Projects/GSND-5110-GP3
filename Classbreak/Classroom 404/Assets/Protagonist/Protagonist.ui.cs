829f3ab0bdcd853f82d6694d42f0251885e7dd70
Nianyi Wang <wangnianyi2001@outlook.com>
2024-11-25 21:53:04 -0500

    Implement the depth fog effect

--- Classbreak/Classroom 404/Assets/Protagonist/Protagonist.ui.cs
+++ Classbreak/Classroom 404/Assets/Protagonist/Protagonist.ui.cs
@@ -10,6 +10,8 @@ namespace Game
 		[SerializeField] private Graphic focus;
 		[SerializeField] private Graphic eyelids;
 		[Min(0)] public float eyelidSpeed = 1.0f;
+		[SerializeField] private Graphic depthFog;
+		[Min(0)] public float depthFogSpeed = 1.0f;
 
 		private float EyelidOpenness
 		{
@@ -44,6 +46,41 @@ namespace Game
 			}
 			EyelidOpenness = targetOpenness;
 			setEyelidOpennessCoroutine = null;
+		}
+
+		private float DepthFogP
+		{
+			get => depthFog.material.GetFloat("_P");
+			set
+			{
+				value = Mathf.Clamp01(value);
+				depthFog.material.SetFloat("_P", value);
+				depthFog.enabled = value != 1.0f;
+			}
+		}
+
+		private Coroutine setDepthFogPCoroutine;
+		public void SetDepthFogP(float targetP)
+		{
+			if(setDepthFogPCoroutine != null)
+			{
+				StopCoroutine(setEyelidOpennessCoroutine);
+				setDepthFogPCoroutine = null;
+			}
+			StartCoroutine(SetDepthFogPCoroutine(targetP));
+		}
+
+		private IEnumerator SetDepthFogPCoroutine(float targetP)
+		{
+			float startP = DepthFogP;
+			float totalTime = Mathf.Abs((startP - targetP) / depthFogSpeed);
+			for(float startTime = Time.time, elapsed; (elapsed = Time.time - startTime) < totalTime;)
+			{
+				DepthFogP = Mathf.Lerp(startP, targetP, elapsed / totalTime);
+				yield return new WaitForEndOfFrame();
+			}
+			DepthFogP = targetP;
+			setDepthFogPCoroutine = null;
 		}
 	}
 }
