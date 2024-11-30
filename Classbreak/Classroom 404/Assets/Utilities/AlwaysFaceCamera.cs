22a40429b1d7f524f9feaf8919f2a81e234ca887
Nianyi Wang <wangnianyi2001@outlook.com>
2024-11-30 01:16:36 -0500

    Make the mouse indicator behave better :D

--- Classbreak/Classroom 404/Assets/Utilities/AlwaysFaceCamera.cs
+++ Classbreak/Classroom 404/Assets/Utilities/AlwaysFaceCamera.cs
@@ -9,5 +9,7 @@ public class AlwaysFaceCamera : MonoBehaviour {
 
 	protected void LateUpdate() {
 		transform.rotation = Quaternion.LookRotation(-mainCam.transform.forward);
+		float distance = Vector3.Distance(mainCam.transform.position, transform.position);
+		transform.localScale = Mathf.Atan(distance) * Vector3.one;
 	}
 }
