6e128a733b4dab1757190cefc99fa6294da70a53
Nianyi Wang <wangnianyi2001@outlook.com>
2024-12-01 18:23:04 -0500

    Rename the script AlwaysFaceCamera to WorldSpaceUi

--- Classroom 404/Assets/Utilities/WorldSpaceUi.cs
+++ Classroom 404/Assets/Utilities/WorldSpaceUi.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class AlwaysFaceCamera : MonoBehaviour {
+public class WorldSpaceUi : MonoBehaviour {
 	static private Camera mainCam;
 
 	protected void Awake() {
@@ -10,6 +10,6 @@ public class AlwaysFaceCamera : MonoBehaviour {
 	protected void LateUpdate() {
 		transform.rotation = Quaternion.LookRotation(-mainCam.transform.forward);
 		float distance = Vector3.Distance(mainCam.transform.position, transform.position);
-		transform.localScale = Mathf.Atan(distance) * Vector3.one;
+		transform.localScale = distance * Vector3.one;
 	}
 }
